c9b07dbdf8814a084d9fd7f6b566c381c05a3dfe
ravikumaryadav2 <raviyadav8070@gmail.com>
2025-02-19 21:58:22 +0530

    web dev

---
+++ Day-24/app.js
@@ -0,0 +1,32 @@
+document.getElementById("name").innerText = "Peter Parker"
+
+// document.getElementById("description").innerText = "lorem20"
+
+//  document.getElementsByClassName("images").innerText = " assets/creation_2.jpeg"
+
+// document.getElementsByTagName("p").innerText = "abc";
+
+
+//  console.dir (document.querySelector("h1"));
+
+// let para = document.querySelector("p");
+// console.dir(para);
+
+
+// let img = document.querySelector("img");
+
+// img.getAttribute("id");
+
+// let heading = document.querySelector("name").style.color = "red";
+// console.log(heading.color = "");
+
+// let links = document.querySelectorAll(".box a");
+
+// for(link of links){
+//     link.style.color = "black ";
+// }
+
+
+// for(let i = 0; i < links.length; i++) {
+//     links[i].style.color = " red";
+// }
